1a3067bc0a878ae2749fdc9e371a170aecab2e4c
Manji27 <marcus.mendy27@gmail.com>
2022-10-18 12:20:35 +0200

    Project Movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Header } from './components';
+import apiMovie, { apiMovieMap } from './conf/api.movie';
+import Films from './features/films';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+      selectedMovie: 0,
+      loaded: false
+    }
+
+  }
+
+  componentDidMount() {
+    apiMovie.get('/discover/movie')
+      .then( response => response.data.results )
+      .then( moviesApi => {
+        const movies = moviesApi.map(apiMovieMap);
+        this.updateMovies(movies);
+      })
+      .catch( err => console.log(err));
+  }
+
+  updateMovies = (movies) => {
+    console.log(movies)
+    this.setState({
+      movies,
+      loaded: true
+    })
+  }
+
+  updateSelectedMovie = (index) => {
+    this.setState({
+      selectedMovie: index
+    })
+  }
+
+  render() {
+    return (
+      <div className="App d-flex flex-column" >
+        <Header />
+        <Films
+          loaded={ this.state.loaded }
+          updateMovies={ this.updateMovies }
+          updateSelectedMovie={ this.updateSelectedMovie }
+          movies={ this.state.movies }
+          selectedMovie={ this.state.selectedMovie }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
